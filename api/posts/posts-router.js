7431953368af235de518e5df4f3030f5f9550a02
MaxwellChrist <ribthulu@gmail.com>
2022-05-10 15:35:14 -0400

    set up server and completed test 1

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,38 @@
 // implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(result => {
+        res.json(result)
+    })
+    .catch(result => {
+        res.status(500).json({ message: "The posts information could not be retrieved"  })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+router.get('/:id/comments', (req, res) => {
+    
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    console.log('Now running!')
+});
+
 // require your posts router and connect it here
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
