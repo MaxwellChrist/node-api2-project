0b5dbe388f0d0d4ffa0276299ab151370cea974b
MaxwellChrist <ribthulu@gmail.com>
2022-05-10 16:03:14 -0400

    post request test completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -15,11 +15,38 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    
+    const id = req.params.id
+    Posts.findById(id)
+    .then(result => {
+        if (!result) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        } else {
+            res.json(result)
+        }
+    })
+    .catch(result => {
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    })
+
 })
 
 router.post('/', (req, res) => {
-    
+    const {title, contents} = req.body
+    console.log(req.body)
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    } else {
+        Posts.insert(req.body)
+        .then(result => {
+            Posts.findById(result.id)
+                .then(result2 => {
+                    res.status(201).json(result2)
+            })
+        })
+        .catch(result => {
+            res.status(500).json({ message: "There was an error while saving the post to the database" })
+        })
+    }
 })
 
 router.put('/:id', (req, res) => {
